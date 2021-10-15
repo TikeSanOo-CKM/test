28352213ce3d781a614c70be551aa187c3b68914
TikeSanOo-CKM <60506199+TikeSanOo-CKM@users.noreply.github.com>
2021-10-15 17:38:08 +0630

    update Template

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -17,12 +17,13 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/taxi.css') }}" rel="stylesheet">
 </head>
 
 <body>
-
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
@@ -33,56 +34,40 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <!-- Styles -->
-                <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-                <link href="{{ asset('css/taxi.css') }}" rel="stylesheet">
-                </head>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-                <body>
-                    <div id="app">
-                        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-                            <div class="container">
-                                <a class="navbar-brand" href="{{ url('/') }}">
-                                    {{ config('app.name', 'Laravel') }}
-                                </a>
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
+                    </ul>
 
-                                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <!-- Left Side Of Navbar -->
-                                    <ul class="navbar-nav mr-auto">
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                Hi There <span class="caret"></span>
+                            </a>
 
-                                    </ul>
-
-                                    <!-- Right Side Of Navbar -->
-                                    <ul class="navbar-nav ml-auto">
-                                        <!-- Authentication Links -->
-                                        <li class="nav-item dropdown">
-                                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                                Hi There <span class="caret"></span>
-                                            </a>
-
-                                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                                <a class="dropdown-item logout" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item logout" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                                    {{ __('Logout') }}
-                                                </a>
+                                    {{ __('Logout') }}
+                                </a>
 
-                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                                    @csrf
-                                                </form>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
                             </div>
-                        </nav>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-                        <main class="py-4">
-                            @yield('content')
-                        </main>
-                    </div>
-                </body>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
 
 </html>
